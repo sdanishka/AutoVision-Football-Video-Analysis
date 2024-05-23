21ff816741675da0b26a3f196bd6cd77e26333ad
Danish Afridi <76814231+sdanishka@users.noreply.github.com>
2024-05-23 19:45:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from utils import read_video, save_video
 from trackers import Tracker
 import cv2
 import numpy as np
-from team_assigner import TeamAssigner
+from .team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
 from view_transformer import ViewTransformer
